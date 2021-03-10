bfc5d3581044c4a92c5eca456d44c09ff329f19c
wenchuyang <writing_happy@163.com>
2021-03-10 14:26:24 +0800

    eventHub 实现任意组件通信

--- reactDemo/src/communication3.jsx
+++ reactDemo/src/communication3.jsx
@@ -1,14 +1,39 @@
+var fnLists = {}
+var eventHub = {
+    trigger(eventName, data) { // 发布
+        let fnList = fnLists[eventName]
+        if(!fnList) { return }
+        for(let i = 0; i<fnList.length; i++) {
+            fnList[i](data)
+        }
+    },
+    on(eventName, fn) {  // 订阅
+        if(!fnLists[eventName]) {
+            fnLists[eventName] = []
+        }
+        fnLists[eventName].push(fn)
+    }
+}
+
+var money = {
+    amount: 10000
+}
+
+
 class Dad1 extends React.Component {
     constructor(props) {
         super(props)
+    }
+    pay() {
+        eventHub.trigger("pay", 10)
     }
     render() {
         return (
             <div className="dad">
                 Dad1 amount: {this.props.amount}
-                <button onClick={this.props.pay}>付款100</button>
-                <Son1 pay={this.props.pay.bind(this)} amount={this.props.amount} />
-                <Son2 pay={this.props.pay.bind(this)} amount={this.props.amount} />
+                <button onClick={this.pay.bind(this)}>付款10</button>
+                <Son1 amount={this.props.amount} />
+                <Son2 amount={this.props.amount} />
             </div>
         )
     }
@@ -17,13 +42,16 @@ class Dad2 extends React.Component {
     constructor(props) {
         super(props)
     }
+    pay() {
+        eventHub.trigger("pay", 50)
+    }
     render() {
         return (
             <div className="dad">
                 Dad2 amount: {this.props.amount}
-                <button onClick={this.props.pay}>付款100</button>
-                <Son3 pay={this.props.pay.bind(this)} amount={this.props.amount} />
-                <Son4 pay={this.props.pay.bind(this)} amount={this.props.amount} />
+                <button onClick={this.pay.bind(this)}>付款50</button>
+                <Son3 amount={this.props.amount} />
+                <Son4 amount={this.props.amount} />
             </div>
         )
     }
@@ -32,10 +60,13 @@ class Son1 extends React.Component {
     constructor(props) {
         super(props)
     }
+    pay() {
+        eventHub.trigger("pay", 100)
+    }
     render() {
         return (
             <div className="son">amount: {this.props.amount}
-                <button onClick={this.props.pay}>付款100</button>
+                <button onClick={this.pay}>付款100</button>
             </div>
         )
     }
@@ -45,10 +76,13 @@ class Son2 extends React.Component {
     constructor(props) {
         super(props)
     }
+    pay() {
+        eventHub.trigger("pay", 200)
+    }
     render() {
         return (
             <div className="son">amount: {this.props.amount}
-                <button onClick={this.props.pay}>付款100</button>
+                <button onClick={this.pay}>付款200</button>
             </div>
         )
     }
@@ -58,10 +92,13 @@ class Son3 extends React.Component {
     constructor(props) {
         super(props)
     }
+    pay() {
+        eventHub.trigger("pay", 300)
+    }
     render() {
         return (
             <div className="son">amount: {this.props.amount}
-                <button onClick={this.props.pay}>付款100</button>
+                <button onClick={this.pay}>付款300</button>
             </div>
         )
     }
@@ -71,10 +108,13 @@ class Son4 extends React.Component {
     constructor(props) {
         super(props)
     }
+    pay() {
+        eventHub.trigger("pay", 400)
+    }
     render() {
         return (
             <div className="son">amount: {this.props.amount}
-                <button onClick={this.props.pay}>付款100</button>
+                <button onClick={this.pay}>付款400</button>
             </div>
         )
     }
@@ -83,23 +123,28 @@ class Son4 extends React.Component {
 class App extends React.Component {
     constructor() {
         super()
-        this.state = {
-            amount: 10000
-        }
-    }
-    pay() {
-        this.setState({
-            amount: this.state.amount - 50
-        })
     }
     render() {
         return (
             <div>
-                <Dad1 pay={this.pay.bind(this)} amount={this.state.amount} />
-                <Dad2 pay={this.pay.bind(this)} amount={this.state.amount} />
+                <Dad1 amount={money.amount} />
+                <Dad2 amount={money.amount} />
             </div>
         )
     }
 }
 
-ReactDOM.render(<App />, document.getElementById("root"))
+function render() {
+    ReactDOM.render(<App />, document.getElementById("root"))
+}
+render()
+
+var x = {
+    init() {
+        eventHub.on("pay", function(data) {
+            money.amount -= data
+            render()
+        })
+    }
+}
+x.init()
